f0e921c2c0266d1d64656872ac316e29fdab2e30
paola <paolatorobejar@gmail.com>
2017-02-01 08:56:45 -0500

    agrupado en funciones

--- js/main.js
+++ js/main.js
@@ -1,49 +1,4 @@
-
-    var nombre= document.getElementById("name").value; 
-    var apellido= document.getElementById("lastname").value;
-
-function validateNombre(evento)
-    {      
-        var array = nombre.split("");
-        var primeraLetra = array[0];
-        var mayus = primeraLetra.toUpperCase();
-        var cortePalabra = false;
-      
-        for(var i=1;i<array.length;i++){
-            if(cortePalabra)
-            {    
-                mayus += array[i].toUpperCase();
-                cortePalabra = false;
-            }
-            else{
-                 mayus+=array[i];
-            }             
-            if(array[i] == " ")
-                cortePalabra = true;            
-            }    
-        document.getElementById("name").value = mayus; 
-    }
-
-
-function validateMayuscula(evento)
-    {   
-        palabra = window.event.keyCode;
-
-        if((palabra>=65 && palabra<=90)||palabra==8||palabra==32){
-            
-        } else {          
-            evento.preventDefault();
-        }
-    }  
-
-
-function validate()
-    {        
-        var bicis=document.getElementById("bicis").value;
-        var correo=document.getElementById("input-email").value;
-        var contraseña=document.getElementById("input-password").value;
-        var biciTipo=true;
-
+ 
         var nom1 =document.createElement("span");
         var nom2 =document.createElement("span");
         var nom3 =document.createElement("span");
@@ -56,35 +11,93 @@ function validate()
             nom2.appendChild(correoText);
             nom3.appendChild(contraText);
             nom4.appendChild(tipoText);
+            
+function validate()
+    { 
 
+    nombreYapellido();
+    biciTipo();
+    correo();
+    contraseña ();
+    validateNombre(evento);
 
+    function nombreYapellido()
+    {
+        var nombre= document.getElementById("name").value; 
+        var apellido= document.getElementById("lastname").value;
+        //Nombre y apellido llenado correctamente
+        if(nombre ==""||apellido=="")
+        {
+            document.getElementById("nombre").appendChild(nom1);
+           
+        } else{
+            document.getElementById("nombre").removeChild(nom1);
+        }
+    }
+
+    function biciTipo()
+    {
+        var bicis=document.getElementById("bicis");
+        var biciTipo=true;
+        
+        //Si bici tiene algo seleccionado
+        if (bicis.value<1)
+        {
+            document.getElementById("tipo").appendChild(nom4);        
+        }else     {
+           
+            document.getElementById("tipo").removeChild(nom4);
+        }
+    } 
+
+    function correo()
+    {
+        var correo=document.getElementById("input-email").value;
+        //Estructura del correo electronico
         if(!/[a-zaA-Z0-9]+[@][a-zA-Z]+[.][a-zA-Z]+/.test(correo))
         {
             document.getElementById("correo").appendChild(nom2);
 
         }else{
-            nom2.style.display="none";
+            document.getElementById("correo").removeChild(nom2);
         }
-        if(contraseña.length < 6 || contraseña === "123456" || contraseña === "098754" || contraseña === "password")
+    }
+
+    function contraseña ()
+    {
+        var contraseña=document.getElementById("input-password").value;
+        
+        //Contraseña no menor a 6 caracteres
+        if(contraseña.length < 6 || contraseña == "123456" || contraseña == "098754" || contraseña == "password")
         {
             document.getElementById("contraseña").appendChild(nom3);
         }else{
-            nom3.style.display="none";
-        }
-        if (bicis<1)
-        {
-            biciTipo=false;
-            document.getElementById("tipo").appendChild(nom4);
-            
-        }else     {
-            biciTipo=true;
-            nom4.style.display="none";
-        }
-        if(nombre ==""||apellido==""||correo==""||contraseña=="")
-        {
-            document.getElementById("nombre").appendChild(nom1);
-           
-        }            
+            console.log("ferfrf");
+            document.getElementById("contraseña").removeChild(nom3);
+        } 
+    }
+
+    function validateNombre(evento)
+        {      
+            var array = nombre.split("");
+            var primeraLetra = array[0];
+            var mayus = primeraLetra.toUpperCase();
+            var corte= false;
+          
+            for(var i=1;i<array.length;i++){
+                if(corte)
+                {    
+                    mayus += array[i].toUpperCase();
+                    corte = false;
+                }
+                else{
+                     mayus+=array[i];
+                }             
+                if(array[i] == " ")
+                    corte= true;            
+                }    
+            document.getElementById("name").value = mayus; 
+        }           
     }
 
 
