6626a3884e38db5a3863d8ebf0359b6f4ef0396f
paola <paolatorobejar@gmail.com>
2017-02-22 12:47:53 -0500

    salida de spans con jquery

--- js/main.js
+++ js/main.js
@@ -1,13 +1,89 @@
+//--------------CODIGO CON JQUERY-------------- 
+function validateForm()
+{
+    validarNombre();
+    validarApellido();
+    validarEmail();
+    validarContrasenia();
+    validarBicicleta();
+}
 
-   function validate()
+function validarNombre()
+{
+    var input=$("#name").val();
+    if(input.length=="")
+    {
+        $("#name").after("<span>Debe ingresar su nombre</span>");
+    }
+    else{
+        $("span").remove();
+    }
+
+}
+function validarApellido()
+{
+    var input=$("#lastname").val();
+    if(input.length=="")
+    {
+        $("#lastname").after("<span>Debe ingresar su apellido</span>");
+    }
+    else{
+        $("span").remove();
+    }
+}
+function validarEmail()
+{
+    var input=$("#input-email").val();
+    if(!/[a-zaA-Z0-9]+[@][a-zA-Z]+[.][a-zA-Z]+/.test(input))
+    {
+        $("#input-email").after("<span>Verifique su e-mail</span>");
+    }
+    else{
+        $("span").remove();
+    }
+}
+function validarContrasenia()
+{
+    var input=$("#input-password").val();
+    if((input).length < 6 || input == "123456" || input== "098754" || input== "password")
+    {
+        $("#input-password").after("<span>Debe ingresar su apellido</span>");
+    }
+    else{
+        $("span").remove();
+    }
+}
+function validarBicicleta()
+{
+    var input=$("#lastname").val();
+    if(input.length=="")
+    {
+        $("#lastname").after("<span>Debe ingresar su apellido</span>");
+    }
+    else{
+        $("span").remove();
+    }
+
+}
+   
+        //var span = $(".form-container").after("<span>Debe ingresar su nombre</span>")
+        
+  
+//-------------CODIGO SOLO JAVASCRIPT----------
+    /* 
+$("#lastname").after("<span>Debe ingresar su apellido</span>");
+                $("#input-email").after("<span>Verifique su email</span>");
+                $("#input-password").after("<span>La contraseña debe tener al menos 6 caractere</span>");
+                $("#tipos").after("<span></span>");
+    function validate()
     { 
         validarNombre();
         validarApellido();
         validarEmail();
         validarContrasenia();
         validarBicicleta();
-    }   
-   function crearNodo(container,mensaje)
+    }
+    function crearNodo(container,mensaje)
     {   
         var spans=container.getElementsByTagName("span");
         if(spans.length==0)
@@ -23,7 +99,8 @@
         if(input.nextSibling!=null)
             input.parentNode.removeChild(input.nextSibling);
     }              
- 
+    
+
     function validarNombre()
     {
         var nombre= document.querySelector(".name-container");
@@ -97,7 +174,5 @@
         {
             eliminarNodo(option);
         }
-    }      
+    }    */  
 
-
- 
